86860ca09f43a6dddd130945175c7d7638d242fd
Coder <64437866+OscarIraira23@users.noreply.github.com>
2020-10-01 15:56:16 -0300

    Update Almacenamiento_datos_T_H.ino

--- Almacenamiento_datos_T_H.ino
+++ Almacenamiento_datos_T_H.ino
@@ -9,21 +9,19 @@
 usada en la línea n° 7
 */
 
-#include <dht.h>
+#include <DHT.h>
 #include <SD.h>
 #include <Wire.h>
 #include "RTClib.h"
-/*
-Si desea usar una pantalla LCD I2C, debe solo "descomentar" la línea que indica el uso de la libreria para LiquidCrystal_I2C
-y también la línea N° 23, la que corresponde a su configuración respecto a la pantalla.
-*/
+#include <LiquidCrystal_I2C.h>
 
-//#include <LiquidCrystal_I2C.h>
-
-//LiquidCrystal_I2C lcd(0x27,16,4);
+LiquidCrystal_I2C lcd(0x27,16,4);
 
 RTC_DS3231 RTC;
-dht DHT;
+
+#define DHTPIN 6
+#define DHTTYPE DHT22
+DHT dht(DHTPIN,DHTTYPE);
 
 #define LOG_INTERVAL 60000
 #define ECHO_TO_SERIAL 1
@@ -33,13 +31,14 @@ const int chipSelect = 10;
 
 File logfile;
 
+float humedad;
+float temperatura;
+
 void setup()
 {
-  /*
-  Si queremos visualizar la información en nuestra pantalla LCD, el usuario debe descomentar el línea respectiva
-   a la que indica pantalla(), pues esta corresponde a una función que inicializa la pantalla como tal.
-   */
-    //pantalla();
+    lcd.init();
+    lcd.backlight();
+
     Serial.begin(9600);
     Serial.print("Inicializando tarjeta SD ... ");
     pinMode(10, OUTPUT);
@@ -179,21 +178,20 @@ void loop()
 
   #endif
 
-  float humedad = DHT.humidity;
-  float tc = DHT.temperature;
-
+  humedad = dht.readHumidity();
+  temperatura = dht.readTemperature();
+  
   if(isnan(tc) || isnan(humedad))
   {
       Serial.println("Fallo en la lectura de sensor DHT");
   }
   else{
-    //svd = 5.018 + 0.32321*tc + 8.1847e-3*pow(tc,2) + 3.1243e-4*pow(tc,3);
-    //vd = humedad/100*svd;
+
     logfile.print(",");
     logfile.print(humedad, 2);
     logfile.print(" %");
     logfile.print(",");
-    logfile.print(tc, 2);
+    logfile.print(temperatura, 2);
 
   #if ECHO_TO_SERIAL
       Serial.print(",");
@@ -203,12 +201,16 @@ void loop()
       Serial.print(tc, 2);
   #endif //ECHO_TO_SERIAL
     }
-    /*
-    Así como en para iniciar el uso de la pantalla debebmos desbloquear su libreria y la función
-    pantalla() , para poder observar los datos en la pantalla LCD, debemos descomentar la línea que cuenta
-    con la función mostrar_datos()
-    */
-  //mostrar_datos(humedad,tc);
+  
+    lcd.setCursor(0,0);
+    lcd.print("Humedad : ");
+    lcd.print(humedad);
+    lcd.print("%");
+    lcd.setCursor(0,1);
+    lcd.print("Temperatura: ");
+    lcd.print(temperatura);
+    lcd.print(" C");
+    delay(1000);
 
   logfile.println();
   #if ECHO_TO_SERIAL
@@ -220,31 +222,3 @@ void loop()
 
 }
 
-  /* A continuación el usuario podrá encontrarse las dos funciones creadas para el uso de la pantalla LCD I2C, nuevamente
-  se reitera que si deseamos hacer uso de la pantalla, debemos descomentar todo lo antes mencionado y también las siguientes
-  funciones
-  */
-
-  /*
-void pantalla()
-  {
-    lcd.init();
-    lcd.init();
-
-    lcd.backlight();
-
-  }
-
-void mostrar_datos(int h , int t)
-  {
-    lcd.setCursor(0,0);
-    lcd.print("Humedad : ");
-    lcd.print(h);
-    lcd.print("%");
-    lcd.setCursor(0,1);
-    lcd.print("Temp: ");
-    lcd.print(t);
-    lcd.print(" C");
-    delay(2000);
-  }
-  */
